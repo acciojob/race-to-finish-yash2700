7613af31c9b1b9e0437621458aaf64adef070e9f
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-22 14:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-window.promises = [];
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min) * 1000; // Convert to milliseconds
+}
 
-// Do not change the code above this
-// add your promises to the array `promises`
+const promises = [];
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const time = getRandomTime(1, 5);
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${time / 1000} seconds`);
+    }, time);
+  });
+  promises.push(promise);
+}
+
+Promise.any(promises)
+  .then((result) => {
+    const output = document.getElementById("output");
+    output.innerText = result;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
